c4096275e5097ff3eae6af0f6b67e2dcb62d3971
Rowena Shi <rowshi2001@live.ca>
2021-08-27 13:37:34 -0400

    finalcommit

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from config import *
 spreadSheet = accessSheet(credentialsFile, accessScope, spreadsheetName)
 dictionary = createDictionary(spreadSheet[1], startCol, startRow, endCol)
 summary = summarize(spreadSheet[1], dictionary)
-
 writeSummary(spreadSheet[0], summary, startCol, startRow, endCol, endRow)
